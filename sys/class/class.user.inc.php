0910b108a9dd8a2415bea4154171d55d927dd732
hamre <bjorn.ax@gmail.com>
2013-11-09 01:18:36 +0100

    commiting

---
+++ sys/class/class.user.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * PHP version 5
+ * 
+ * LICENSE: This source file is subject to the MIT License, available
+ * at http://www.opensource.org/licenses/mit-license.html
+ * 
+ * @author      Björn Ax <bjorn.ax@gmail.com>
+ * @copyright   2013 Björn Ax
+ * @license     http://www.opensource.org/licenses/mit-license.html  
+ */
+
+class user extends DB_connect {
+
+
+}
